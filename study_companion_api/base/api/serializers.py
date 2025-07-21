55a301e6421d29c8d61ac97e38339a0300e25ea4
IshaaqDaahir <ishaqtafiyau@gmail.com>
2025-07-21 16:29:29 +0100

    Update Error Message display

--- study_companion_api/base/api/serializers.py
+++ study_companion_api/base/api/serializers.py
@@ -19,10 +19,19 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=User.objects.all(),
+        message="This email is already registered.")]
+    )
+    username = serializers.CharField(
+    required=True,
+    validators=[UniqueValidator(
+        queryset=User.objects.all(),
+        message="This username is already taken."
+        )]
     )
     password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    
 
     class Meta:
         model = User
